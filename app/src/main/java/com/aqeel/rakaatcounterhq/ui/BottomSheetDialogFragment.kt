425f02060f08e0504e8a64b7c83fa2b440bf6b02
aqeel <aqeul@Aqeel>
2024-07-14 16:41:21 +0300

    اضافة تخصيص الصلاة

--- app/src/main/java/com/aqeel/rakaatcounterhq/ui/BottomSheetDialogFragment.kt
+++ app/src/main/java/com/aqeel/rakaatcounterhq/ui/BottomSheetDialogFragment.kt
@@ -1,9 +1,11 @@
 package com.aqeel.rakaatcounterhq.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.aqeel.rakaatcounterhq.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -38,6 +40,35 @@ class UniversalBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
             }
             R.layout.bottom_sheet_prayers_list -> {
+
+            }
+            R.layout.bottom_sheet_manual_customize -> {
+
+                var rakaat2 = view.findViewById<Button>(R.id.rakaat2)
+                var rakaat50 = view.findViewById<Button>(R.id.rakaat_50)
+                var rakaat100 = view.findViewById<Button>(R.id.rakaat_100)
+                var numberPicker = view.findViewById<com.shawnlin.numberpicker.NumberPicker>(R.id.number_picker)
+                var startButton = view.findViewById<Button>(R.id.start_prayer)
+
+                rakaat2.setOnClickListener {
+                    numberPicker.value = 2
+                }
+                rakaat50.setOnClickListener {
+                    numberPicker.value = 50
+                }
+                rakaat100.setOnClickListener {
+                    numberPicker.value = 100
+                }
+                startButton.setOnClickListener {
+                    // Start the prayer
+                    val intent= Intent(context,PrayerActivity::class.java)
+                    intent.putExtra("prayer","صلاة مخصصة")
+                    intent.putExtra("image","added_ic")
+                    intent.putExtra("rakaars",numberPicker.value)
+                    context?.startActivity(intent)
+                }
+
+
 
             }
 
